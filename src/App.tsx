074b710e407c0c29c962f4cee8e9a1a56d1524e7
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-03-09 01:06:35 +0100

    add sound + lighting effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { Howl } from 'howler';
 import CanvasManipulator from "./CanvasManipulator";
 import './App.css';
-
+  const sound = new Howl({
+    src: ['/mixkit-thunder-deep-rumble-1296.mp3'],
+    loop: true,
+    volume: 0.5,
+    autoplay: true
+  });
 
 function App() {
   return (
     <div className="App">
+      <div className="bg"></div>
+      <div className="lightning flashit"></div>
       <header className="App-header">
         <CanvasManipulator />
+        <div className="buttons">
+          <button onClick={() => sound.play()}>Play</button>
+          <button onClick={() => sound.stop()}>Mute</button>
+        </div>
       </header>
     </div>
   );
